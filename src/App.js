487c2f0d7270506008d5e6782d031b8194dc87b0
petrborr <petrborr@gmail.com>
2022-12-26 22:43:44 +0200

    add: numerical input changes icon's class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("")
+  const validator = (number) => {
+    return (number.trim() !== '') && !isNaN(number)
+  }
+  const isValidNumber = useMemo(()=>validator(text), [text])
+  
   return (
     <div className="App">
      <div className="control has-icons-right">
@@ -8,10 +15,14 @@ function App() {
           className="input is-large"
           type="text"
           placeholder="Enter number..."
+          value={text}
+          onChange={(t) => {
+            setText(t.target.value)
+          }}
 
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
+          <i className={`fas ${isValidNumber ? "fa-check":"fa-times"}`}/>
         </span> 
       </div>
     </div>
